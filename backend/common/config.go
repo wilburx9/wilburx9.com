158c5a4ff5e986b8c3d174fe96cb41761c3c12ac
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2021-04-11 11:33:02 +0100

    refactor: setup loading of config

---
+++ backend/common/config.go
@@ -0,0 +1,38 @@
+package common
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+// Config is global object that holds all application level variables.
+var Config appConfig
+
+type appConfig struct {
+	ServerPort        string `mapstructure:"server_port"`
+	MediumUsername    string `mapstructure:"medium_username"`
+	WPUrl             string `mapstructure:"wp_url"`
+	UnsplashUsername  string `mapstructure:"unsplash_username"`
+	UnsplashAccessKey string `mapstructure:"unsplash_access_key"`
+	Env               string `mapstructure:"env"`
+}
+
+// LoadConfig loads config variables from a config file or environment variables
+func LoadConfig(path string) error {
+	v := viper.New()
+	v.SetConfigName("config")
+	v.SetConfigType("yaml")
+	v.SetEnvPrefix("WilburX9")
+	v.AutomaticEnv()
+	v.AddConfigPath(path)
+
+	if err := v.BindEnv("server_port", "PORT"); err != nil {
+		return fmt.Errorf("unable to bind ServerPort to Port env: %s", err)
+	}
+
+	if err := v.ReadInConfig(); err != nil {
+		return fmt.Errorf("failed to read the configuration file: %s", err)
+	}
+
+	return v.Unmarshal(&Config)
+}
--- backend/main.go
+++ backend/main.go
@@ -5,14 +5,20 @@ import (
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/wilburt/wilburx9.dev/backend/articles"
+	"github.com/wilburt/wilburx9.dev/backend/common"
 	"github.com/wilburt/wilburx9.dev/backend/gallery"
 	"net/http"
 )
 
 func main() {
+	// Attempt to load config
+	if err := common.LoadConfig("../"); err != nil {
+		panic(fmt.Errorf("invalid application configuration: %s", err))
+	}
 	gin.ForceConsoleColor()
-
+	gin.SetMode(common.Config.Env)
 	router := gin.Default()
+
 	// Serve frontend static files
 	router.Use(static.Serve("/", static.LocalFile("../frontend/build", true)))
 
@@ -21,7 +27,6 @@ func main() {
 	api.GET("/articles", articles.Handler)
 	api.GET("/gallery", gallery.Handler)
 
-	var port = "8080" // TODO: Load dynamically
-	s := &http.Server{Addr: fmt.Sprintf(":%s", port), Handler: router}
+	s := &http.Server{Addr: fmt.Sprintf(":%s", common.Config.ServerPort), Handler: router}
 	s.ListenAndServe()
 }
