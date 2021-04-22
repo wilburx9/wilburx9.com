d7593c0c75e4a34f4169ebeecff6b0ed65bcbb36
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2021-04-22 02:13:45 +0100

    chore: added instagram skeleton code

---
+++ backend/gallery/instagram.go
@@ -0,0 +1,14 @@
+package gallery
+
+import (
+	"github.com/wilburt/wilburx9.dev/backend/common"
+)
+
+type instagram struct {
+
+}
+
+func (i instagram) fetchImages(client common.HttpClient) []Image {
+	panic("implement me")
+}
+
