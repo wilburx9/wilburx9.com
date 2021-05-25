2b0a65c607d3fc44394286125fbd80985d77d851
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2021-05-25 02:56:13 +0100

    feat: implemented fetching of Github repos

--- backend/api/repos/repos.go
+++ backend/api/repos/repos.go
@@ -3,28 +3,11 @@ package repos
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/wilburt/wilburx9.dev/backend/api/internal"
-	"time"
 )
 
 // Handler retrieves a list of all git repos, sorted in descending stars and forks
 func Handler(c *gin.Context) {
 
-}
-
-// repo represents a single git repository
-type repo struct {
-	Name        string    `json:"name"`
-	Stars       int       `json:"stars"`
-	Forks       int       `json:"forks"`
-	Url         string    `json:"url"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	License     string    `json:"license"`
-	Languages   []struct {
-		Name  string `json:"name"`
-		Color string `json:"color"`
-	} `json:"languages"`
 }
 
 func getCacheKey(suffix string) string {
