4692d00eae46e211683fbd09f8ba50586d582929
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2021-05-25 03:08:21 +0100

    feat: completed repos endpoint

--- backend/api/repos/repos.go
+++ backend/api/repos/repos.go
@@ -1,13 +1,38 @@
 package repos
 
 import (
+	"encoding/json"
+	"github.com/dgraph-io/badger/v3"
 	"github.com/gin-gonic/gin"
 	"github.com/wilburt/wilburx9.dev/backend/api/internal"
+	"github.com/wilburt/wilburx9.dev/backend/api/repos/internal/models"
+	"net/http"
+	"sort"
 )
 
 // Handler retrieves a list of all git repos, sorted in descending stars and forks
 func Handler(c *gin.Context) {
+	fetch := internal.Fetch{
+		Db:         c.MustGet(internal.Db).(*badger.DB),
+		HttpClient: &http.Client{},
+	}
 
+	github := Github{Auth: internal.Config.GithubToken, Username: internal.Config.GithubUsername, Fetch: fetch}
+	fetchers := [...]internal.Fetcher{github}
+
+	var allRepos = make([]models.Repo, 0)
+	for _, f := range fetchers {
+		var repos []models.Repo
+		bytes, _ := f.GetCached()
+		json.Unmarshal(bytes, &repos)
+		allRepos = append(allRepos, repos...)
+	}
+
+	// Sort in descending order of scores
+	sort.Slice(allRepos, func(i, j int) bool {
+		return allRepos[i].Score() > allRepos[j].Score()
+	})
+	c.JSON(http.StatusOK, internal.MakeSuccessResponse(allRepos))
 }
 
 func getCacheKey(suffix string) string {
