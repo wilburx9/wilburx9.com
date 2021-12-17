3439dae45e8c693d0eb1c4ad2d9d8757e340f2ee
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2021-12-17 02:37:47 +0100

    feat: added links to the images

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,6 +1,12 @@
-import {extendTheme, theme as base} from "@chakra-ui/react";
+import {extendTheme, theme as base, ThemeConfig} from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+}
 
 const theme = extendTheme({
+  config,
   fonts: {
     heading: `'Roboto Mono', monospace, ${base.fonts?.heading}`,
     body: `'Roboto Mono', monospace, ${base.fonts?.body}`
