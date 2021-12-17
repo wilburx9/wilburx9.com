2e8eca6bd9923674e66a1573a2478ca376568d62
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2021-12-17 02:05:13 +0100

    fix: fixed border issue

--- frontend/src/ArticleComponent.tsx
+++ frontend/src/ArticleComponent.tsx
@@ -1,4 +1,4 @@
-import {Box, Flex, Heading, Image, Text, useColorModeValue, VStack} from "@chakra-ui/react";
+import {Box, Flex, Heading, Image, Stack, StackDivider, Text, useColorModeValue, VStack} from "@chakra-ui/react";
 import React, {useContext, useEffect} from "react";
 import {DataContext} from "./DataProvider";
 import {Utils} from "./Utils";
@@ -12,28 +12,30 @@ export const ArticleComponent = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchArticles])
 
-  let borderColor = useColorModeValue('gray.400', 'red.600');
+  let borderColor = useColorModeValue('gray.400', 'gray.600');
 
   if (!articles) return <Box/>
 
   let groups = Utils.groupArray(articles, 2);
   return (
-    <Flex direction={{base: 'column-reverse', md: 'row'}}>
-      {groups.map((group, index) =>
-        <Box
-          borderLeftColor={borderColor} // TODO: Not working. Fix!
-          borderLeft={{base: '0px', md: (index === 0) ? '0px' : '1px'}}
-          w={{base: 'full', md: '50%'}}>
-          <VStack key={group[0].title}>
-            {group.map(function (article, index) {
-              let isLast = index === (group.length - 1)
-              return <Box w="full" pb={isLast ? 0 : 4} px={4}>
-                <Column {...article} key={article.title}/>
-              </Box>;
-            })}
-          </VStack>
-        </Box>)}
-    </Flex>
+    <Stack
+      direction={{base: 'column', md: 'row'}}
+      divider={<StackDivider
+        display={{base: 'none', md: 'inherit'}}
+        borderColor={borderColor}/>}>
+
+      {groups.map((group) => <Box
+        w={{base: 'full', md: '50%'}}>
+        <VStack key={group[0].title}>
+          {group.map(function (article, index) {
+            let isLast = index === (group.length - 1)
+            return <Box w="full" pb={isLast ? 0 : 4} px={4}>
+              <Column {...article} key={article.title}/>
+            </Box>;
+          })}
+        </VStack>
+      </Box>)}
+    </Stack>
   )
 }
 
