356a6afc632cd2840956a095d4f95381118c6ca7
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2021-12-18 18:16:07 +0100

    refactor: minor UI improvements

--- frontend/src/components/ArticlesComponent.tsx
+++ frontend/src/components/ArticlesComponent.tsx
@@ -4,8 +4,7 @@ import {
   Image,
   LinkBox, LinkOverlay,
   Text,
-  VStack,
-  SimpleGrid, Icon, Circle, useColorModeValue, AspectRatio,
+  SimpleGrid, Icon, Circle, useColorModeValue, AspectRatio, Flex,
 } from "@chakra-ui/react";
 import React, {useContext, useEffect} from "react";
 import {DataContext} from "../DataProvider";
@@ -17,24 +16,23 @@ export const ArticlesComponent = () => {
 
   useEffect(() => {
     fetchArticles?.()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchArticles])
 
   if (!articles) return <Box/>
 
   return (
-    <SimpleGrid columns={{base: 1, md: 2, lg: 3}} spacing={10} px={[5, null, 10]} py={4}>
+    <SimpleGrid columns={{base: 1, md: 2, lg: 3}} spacing={10} py={4}>
       {articles.map((article) => <Column {...article} key={article.title}/>)}
     </SimpleGrid>
   )
 }
 
-
 function Column(props: ArticleModel) {
   return <LinkBox>
-    <LinkOverlay href={props.url} isExternal>
-      <Box w='full' h='full' borderRadius='lg' borderWidth='1px' pb={5}>
-        <VStack align='start' justify='space-between' h='full' spacing={0}>
+    <LinkOverlay href={props.url} isExternal role="group">
+      <Box w='full' h='full' borderRadius='lg' borderWidth='1px' pb={5}
+           _groupHover={{bg: useColorModeValue('gray.100', 'gray.900')}}>
+        <Flex direction='column' align='start' justify='space-between' h='full' spacing={0}>
           <AspectRatio w='full' ratio={3 / 2}>
             <Image
               src={props.thumbnail}
@@ -45,12 +43,12 @@ function Column(props: ArticleModel) {
           </AspectRatio>
           <Heading px={6} pt={6} fontSize="20px" align="start" fontWeight="bold">{props.title}</Heading>
           <Text px={6} pt={3} noOfLines={3} fontSize="16px" align="start" fontWeight="regular">{props.excerpt}</Text>
-          <Box placeSelf='end'>
+          <Box ml='auto'>
             <Circle bg={useColorModeValue('gray.200', 'gray.700')} boxSize={10} mt={4} mx={9}>
               <Icon as={HiArrowRight} color={useColorModeValue('gray.800', 'gray.200')}/>
             </Circle>
           </Box>
-        </VStack>
+        </Flex>
       </Box>
     </LinkOverlay>
   </LinkBox>
