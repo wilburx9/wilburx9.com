a5f8524edcaa653c4b57a5eb23ab91d6c312254c
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2021-12-23 22:17:16 +0100

    feat: improved contact form

--- frontend/src/components/ContactComponent.tsx
+++ frontend/src/components/ContactComponent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Box, Button, FormControl, HStack, Input, Textarea, useColorModeValue, VStack} from "@chakra-ui/react";
+import {Box, Button, FormControl, Heading, HStack, Input, Textarea, useColorModeValue, VStack} from "@chakra-ui/react";
 import {HiOutlineArrowRight} from "react-icons/all";
 import HCaptcha from '@hcaptcha/react-hcaptcha';
 
@@ -9,25 +9,33 @@ export const ContactComponent = () => {
 
   }
 
+  let bColor = useColorModeValue('black', 'white');
+  let focusStyle = {borderColor: bColor, boxShadow: 'none', borderWidth: "1px"}
   return <Box mt={4} my={6} py={6} px={20} borderRadius='xl' bg={useColorModeValue('gray.100', 'gray.900')}>
     <FormControl>
-      <HStack alignItems='flex-end'>
+      <Heading mb={4} size='lg' align="start">&#47;&#47;Let's work together</Heading>
+      <HStack alignItems='flex-end' spacing={10}>
 
-        <VStack flexGrow={1}>
-          <HStack w='full'>
-            <Input id='name' autoComplete='name' type='text' placeholder='Name' isInvalid={false} isRequired={true}/>
-            <Input id='email' autoComplete='email' type='email' placeholder='Email'
-                   isInvalid={false} isRequired={true}/>
+        <VStack flexGrow={3} spacing={6}>
+          <HStack w='full' spacing={6}>
+            <Input _focus={focusStyle} id='name' autoComplete='name' type='text'
+                   placeholder='Name' variant='filled' isInvalid={false} isRequired={true}/>
+            <Input _focus={focusStyle} id='email' autoComplete='email' type='email'
+                   placeholder='Email' variant='filled' isInvalid={false} isRequired={true}/>
           </HStack>
-          <Input id='subject' type='text' placeholder='Subject' isInvalid={false} isRequired={true}/>
-          <Textarea id='message' resize='vertical' placeholder='Message' isInvalid={false} isRequired={true}/>
+
+          <Input _focus={focusStyle} id='subject' type='text' placeholder='Subject'
+                 variant='filled' isInvalid={false} isRequired={true}/>
+          <Textarea _focus={focusStyle} id='message' resize='vertical' placeholder='Message'
+                    variant='filled' isInvalid={false} isRequired={true}/>
         </VStack>
 
-        <VStack>
+        <VStack spacing={6}>
           <HCaptcha sitekey={process.env.REACT_APP_H_CAPTCHA_SITE_KEY!}
-                    theme={useColorModeValue('light', 'dark')}
+                    theme={useColorModeValue('light', 'dark')} size='normal'
                     onVerify={(token) => handleCaptchaSuccess(token)}/>
-          <Button isLoading={false} type='submit' loadingText='Sending' spinnerPlacement='end'
+          <Button color='white' isLoading={false} type='submit' borderRadius='lg' loadingText='Sending'
+                  spinnerPlacement='end'
                   rightIcon={<HiOutlineArrowRight/>} w='full' disabled={false}>
             Send
           </Button>
