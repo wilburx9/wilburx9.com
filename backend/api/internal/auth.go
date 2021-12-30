469eef75b5cf2d1c41375f00d35f6887344bca4a
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2021-12-30 18:12:20 +0100

    chore: added cloud functions for Firebase

--- backend/api/internal/auth.go
+++ backend/api/internal/auth.go
@@ -1,14 +1,8 @@
 package internal
 
 import (
-	"crypto/sha512"
-	"encoding/hex"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"golang.org/x/crypto/pbkdf2"
 	"net/http"
-	"strings"
 )
 
 // AuthMiddleware validates that the request has a valid authorization
@@ -28,18 +22,22 @@ func valid(a string) bool {
 		return false
 	}
 
-	v := gen()
-	vs := strings.Split(v, ":")
-
-	// key := []byte(vs[0])
-	auth := pbkdf2.Key([]byte(vs[0]), []byte(vs[1]), 200000, 50, sha512.New)
-	return hex.EncodeToString(auth) == vs[2]
+	// v := gen()
+	// vs := strings.Split(v, ":")
+	//
+	// // key := []byte(vs[0])
+	// auth := pbkdf2.Key([]byte(vs[0]), []byte(vs[1]), 200000, 50, sha512.New)
+	// return hex.EncodeToString(auth) == vs[2]
+	return true
 }
 
-func gen() string {
-	key := uuid.NewString()
-	salt := uuid.NewString()
-	hash := pbkdf2.Key([]byte(key), []byte(salt), 200000, 50, sha512.New)
-	hashStr := hex.EncodeToString(hash)
-	return fmt.Sprintf("%v:%v:%v", key, salt, hashStr)
-}
+// func gen() string {
+// 	key := uuid.NewString()
+// 	salt := uuid.NewString()
+// 	hash := pbkdf2.Key([]byte(key), []byte(salt), 200000, 50, sha512.New)
+// 	hashStr := hex.EncodeToString(hash)
+// 	return fmt.Sprintf("%v:%v:%v", key, salt, hashStr)
+// }
+
+// Key:: 8E4FA332-5894-41FA-A88F-DD76881144F9
+// Salt:: E9E36CE6-CF24-4D5F-8567-848F999C4C7A
