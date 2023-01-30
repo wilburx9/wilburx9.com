af86dae25b17ba65b3b7bdabb1d9c6f085d286bc
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2023-01-31 00:53:49 +0800

    refactor: massive cleanup

--- frontend/src/analytics/keys.ts
+++
@@ -1,9 +0,0 @@
-export const AnalyticsKey = {
-  reason: 'reason',
-  statusCode: 'status_code',
-  url: 'url',
-  message: 'message',
-  rawResponse: 'raw_response',
-  method: 'method',
-  data: 'data',
-}
