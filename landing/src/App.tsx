8532019962ef55e8b49bc45b6dbabf19bcfdb51d
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2023-02-28 21:31:09 +0800

    chore: created landing page

---
+++ landing/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
