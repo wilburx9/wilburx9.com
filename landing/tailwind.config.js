d2de1e20e603e212f6b007f015a1c0bbe515fa40
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2023-02-28 22:08:41 +0800

    feat: added Poppins font

--- landing/tailwind.config.js
+++ landing/tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['Poppins', ...defaultTheme.fontFamily.sans]
+    }
   },
   plugins: [],
 }
