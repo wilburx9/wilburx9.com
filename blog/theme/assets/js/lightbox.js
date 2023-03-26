937d4d2a2801b6a61c6a3bcd3b62376d72ef82d5
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2023-03-26 23:06:15 +0800

    feat: supported lightbox for images

--- blog/theme/assets/js/lightbox.js
+++ blog/theme/assets/js/lightbox.js
@@ -1,23 +1,36 @@
 !function () {
     const images = document.querySelectorAll(".kg-image-card img");
-    images.forEach(function (image) {
+    for (let i = 0; i < images.length; i++) {
+        let image = images[i]
+        let lightBoxId = `lightbox__photo__${i}`
         let imgWrapper = document.createElement("div")
-        imgWrapper.style.backgroundImage = "url(" + image.currentSrc + ")"
+        imgWrapper.style.backgroundImage = `url("${image.currentSrc || image.src}")`
         let container = image.parentElement
         let imgW = Number(image.getAttribute("width"))
         let imgH = Number(image.getAttribute("height"))
+        let imgAspectRatio = `${imgW}/${imgH}`
         let containerW = imgW;
 
         if ((imgW / imgH) < 0.80) containerW = imgH * 0.80
 
         let maxSide = Math.max(containerW, imgH);
         let minSide = Math.min(containerW, imgH);
-        container.style.aspectRatio = maxSide + "/" + minSide
-        image.style.aspectRatio = imgW + "/" + imgH
+        container.style.aspectRatio = `${maxSide}/${minSide}`
+        image.style.aspectRatio = imgAspectRatio
 
 
         container.insertBefore(imgWrapper, image.parentElement.firstChild)
         imgWrapper.append(image)
-        imgWrapper.insertAdjacentHTML("afterbegin", "<span id='zoom-handle'><svg width=\"20\" height=\"20\" fill=\"none\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"m19 19-4.35-4.35M9 6v6M6 9h6m5 0A8 8 0 1 1 1 9a8 8 0 0 1 16 0Z\"/></svg></span>")
-    })
+        imgWrapper.insertAdjacentHTML("afterbegin", `<span onclick='showLightBox("${lightBoxId}");' class='photo-zoom-handle'><svg width="20" height="20" fill="none"><path stroke-linecap="round" stroke-linejoin="round" d="m19 19-4.35-4.35M9 6v6M6 9h6m5 0A8 8 0 1 1 1 9a8 8 0 0 1 16 0Z"/></svg></span>`)
+        container.insertAdjacentHTML("beforebegin", `<div class='photo-lightbox' id='${lightBoxId}'><div style="aspect-ratio: ${imgAspectRatio};"><span onclick='closeLightBox("${lightBoxId}");' class='photo-zoom-handle'><svg width="20" height="20" fill="none"><path stroke-linecap="round" stroke-linejoin="round" d="m19 19-4.35-4.35M6 9h6m5 0A8 8 0 1 1 1 9a8 8 0 0 1 16 0Z"/></svg></span><img src="${image.src}" alt="${image.alt}" style="aspect-ratio: ${imgAspectRatio}"/></div></div>`)
+    }
 }();
+
+function closeLightBox(id) {
+    document.getElementById(id).style.display = "none"
+}
+
+function showLightBox(id) {
+    console.log(`Clicked :: ${id}`)
+    document.getElementById(id).style.display = 'flex'
+}
